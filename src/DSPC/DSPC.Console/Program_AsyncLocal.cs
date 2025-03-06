3cb3d96df7b8bbcd08d72b1a30eaba1af256a7d4
Eugene Titarchuk <etitarchuk@gmail.com>
2025-03-07 01:30:18 +0200

    added consumer producer example

--- src/DSPC/DSPC.Console/Program_AsyncLocal.cs
+++ src/DSPC/DSPC.Console/Program_AsyncLocal.cs
@@ -1,40 +1,40 @@
-﻿using System.Diagnostics;
-using System.Threading;
+﻿//using System.Diagnostics;
+//using System.Threading;
 
-public class Program
-{
-    static AsyncLocal<decimal?> _asyncLocal = new AsyncLocal<decimal?>();
+//public class Program
+//{
+//    static AsyncLocal<decimal?> _asyncLocal = new AsyncLocal<decimal?>();
 
-    public static void Main()
-    {
-        var stopwatch = Stopwatch.StartNew();
+//    public static void Main()
+//    {
+//        var stopwatch = Stopwatch.StartNew();
 
-        _asyncLocal.Value = 200;
+//        _asyncLocal.Value = 200;
 
 
 
-        var options = new ParallelOptions { MaxDegreeOfParallelism = 1 };
-        Parallel.For(0, 100, options, /* async */ i => 
-        {
-            var currentValue = _asyncLocal.Value;
+//        var options = new ParallelOptions { MaxDegreeOfParallelism = 1 };
+//        Parallel.For(0, 100, options, /* async */ i => 
+//        {
+//            var currentValue = _asyncLocal.Value;
 
-            _asyncLocal.Value = Compute(i);
-        });
+//            _asyncLocal.Value = Compute(i);
+//        });
 
-        var currentValue = _asyncLocal.Value;
+//        var currentValue = _asyncLocal.Value;
 
-        Console.WriteLine($"It took: {stopwatch.ElapsedMilliseconds}ms to run");
-    }
+//        Console.WriteLine($"It took: {stopwatch.ElapsedMilliseconds}ms to run");
+//    }
 
-    static Random random = new Random();
-    static decimal Compute(int value)
-    {
-        var randomMilliseconds = random.Next(10, 50);
-        var end = DateTime.Now + TimeSpan.FromMilliseconds(randomMilliseconds);
+//    static Random random = new Random();
+//    static decimal Compute(int value)
+//    {
+//        var randomMilliseconds = random.Next(10, 50);
+//        var end = DateTime.Now + TimeSpan.FromMilliseconds(randomMilliseconds);
 
-        // just use CPU for some period of time
-        while (end > DateTime.Now) { }
+//        // just use CPU for some period of time
+//        while (end > DateTime.Now) { }
 
-        return value + 0.5m;
-    }
-}
+//        return value + 0.5m;
+//    }
+//}
---
+++ src/DSPC/DSPC.Console/Program_Consumer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Concurrent;
+
+class Program
+{
+    static ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+    static SemaphoreSlim semaphore = new SemaphoreSlim(0);
+
+    static async Task Producer()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            queue.Enqueue(i);
+            Console.WriteLine($"Produced: {i}");
+            semaphore.Release(); // signal consumer
+
+            await Task.Delay(500); // do something important
+        }
+    }
+
+    static async Task Consumer()
+    {
+        while (true)
+        {
+            await semaphore.WaitAsync(); // wait for new items in queue
+
+            if (queue.TryDequeue(out int item))
+            {
+                Console.WriteLine($"Consumed: {item}");
+                await Task.Delay(1000); // process items
+            }
+        }
+    }
+
+    static async Task Main()
+    {
+        var producerTask = Producer();
+        var consumerTask = Consumer();
+
+        await Task.WhenAll(producerTask, consumerTask);
+    }
+}
